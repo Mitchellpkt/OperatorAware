1011347bcb01ff5c428a2aec58eb92ecf3e119d7
MitchellPKT <MitchellPKT@protonmail.com>
2018-06-15 10:12:53 -0500

    MVP works with Call 1

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
+import operator_aware_lib_v1
 
 @app.route('/')
 @app.route('/index')
@@ -7,5 +8,8 @@ def index():
     return render_template('index.html')
 
 @app.route('/', methods=['POST'])
+@app.route('/index', methods=['POST'])
 def dummmy():
-    return render_template('output.html')
+    x = request.form['Item_1']
+    OA_eval = operator_aware_lib_v1.evaluate_call(x)
+    return OA_eval
