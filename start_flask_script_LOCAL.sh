67fc1010036d3876543097ba60df75eb8cf9df11
MitchellPKT <MitchellPKT@protonmail.com>
2018-06-19 15:39:34 -0500

    updated local & open scripts to start export vars and start flask

---
+++ start_flask_script_LOCAL.sh
@@ -0,0 +1,5 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/Dropbox/Projects/OperatorAware/GitPath/operatoraware/OperatorAware-5f653aaf3399.json"
+
+export FLASK_APP=OperatorAwareFlaskTop.py
+
+flask run
---
+++ start_flask_script_OPEN.sh
@@ -0,0 +1,5 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/Dropbox/Projects/OperatorAware/GitPath/operatoraware/OperatorAware-5f653aaf3399.json"
+
+export FLASK_APP=OperatorAwareFlaskTop.py
+
+flask run --host 0.0.0.0 --port 2600
