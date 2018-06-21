f417548ab29be27561d306aa2afcbe4b50926567
PwnMeComputer <mitchellpkt@protonmail.com>
2018-06-21 11:45:04 -0500

    fixed path on host device

--- InstallScript_RunOnce.sh
+++ InstallScript_RunOnce.sh
@@ -3,10 +3,22 @@
 echo pip incoming...
 sudo apt-get install python-pip
 
+######
+# Do we need a virtual environment?
+# echo virtualenv incoming...
+# sudo apt-get install virtualenv
+# virtualenv Insight
+# something something activate
+
+pip install conda
+
 echo flask incoming...
 sudo apt install python3-flask
 
+echo snag net-tools
 sudo apt install net-tools # for ifconfig
 
+echo gcloud stuff
+pip install google-cloud #?
 pip install --upgrade gcloud
 pip install --upgrade google-api-python-client
