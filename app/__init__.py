79a8434025f5d174bb7dc7040ab1e139eebf6e4b
neptuneresearch <39543276+neptuneresearch@users.noreply.github.com>
2018-06-27 23:38:15 -0400

    Enable Flask Debug Logging

--- app/__init__.py
+++ app/__init__.py
@@ -2,5 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Start Log
+log_path = "oaflask.log"
+logHandler = RotatingFileHandler(log_path, maxBytes=10000, backupCount=1) 
+formatter = logging.Formatter('[%(asctime)s] {%(pathname)s - %(levelname)s - %(filename)s - %(module)s - %(funcName)s - %(message)s') 
+logHandler.setFormatter(formatter) 
+app.logger.addHandler(logHandler) 
+app.logger.setLevel(logging.DEBUG)
+
 from app import routes
 
