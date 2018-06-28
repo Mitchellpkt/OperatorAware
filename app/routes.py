9b80c40350bec998bc719f8a91d0dfacb5f36c86
MitchellPKT <MitchellPKT@protonmail.com>
2018-06-27 22:02:56 -0500

    added style on output page, alse removed html in routing

--- app/routes.py
+++ app/routes.py
@@ -13,7 +13,6 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 def index():
     return render_template('index.html')
 
-@app.route('/pentest.html')
 @app.route('/pentest')
 def pentest():
     return render_template('pentest.html')
