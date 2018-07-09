97e0f6306769bee68f9fa119c4d458d497d4f5dc
pwn_me_user <mitchellpkt@protonmail.com>
2018-07-09 10:54:43 -0500

    sync

--- start_flask_script_OPEN_DEV.sh
+++ start_flask_script_OPEN_DEV.sh
@@ -5,4 +5,5 @@ export GOOGLE_APPLICATION_CREDENTIALS="/home/pwn_me_user/operatoraware/OperatorA
 export FLASK_APP=OperatorAwareFlaskTop.py
 
 export FLASK_ENV=development
+
 flask run --host 0.0.0.0 --port 2600
